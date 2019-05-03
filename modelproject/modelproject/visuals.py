984287023d825ee0c5f7253ac650ec113f50a961
Tobias T. Ersted <tobi_ersted@hotmail.com>
2019-05-03 22:32:54 +0200

    Small change

--- modelproject/modelproject/visuals.py
+++ modelproject/modelproject/visuals.py
@@ -7,26 +7,6 @@ from .models import *
 def fig_func(epsilon, optimal_h_list, optimal_h_list_1, net_wage, net_wage_1):
     fig = plt.figure(figsize=(8,8));
     ax = fig.add_subplot(1,1,1);
-    # wage_list = list(range(1,50))
-    # optimal_h_list = []
-    # optimal_h_list_1 = []
-    # net_wage = []
-    # net_wage_1 = []
-
-    # for wage in wage_list:
-    #     if round(epsilon,2) == -1:
-    #         continue
-    #     else:
-    #         opt_h, opt_x, opt_u = sol_func(tw=0.2, w=wage, epsilon=round(epsilon,2));
-    #         optimal_h_list.append(opt_h);
-    #         net_wage.append(wage-wage*0.2);
-    # for wage in wage_list:
-    #     if round(epsilon,2) == -1:
-    #         continue
-    #     else:
-    #         opt_h_1, opt_x_1, opt_u_1 = sol_func(tw=0.5, w=wage, epsilon=round(epsilon,2));
-    #         optimal_h_list_1.append(opt_h_1);
-    #         net_wage_1.append(wage-wage*0.2);
     ax.plot(optimal_h_list_1,net_wage_1,lw=2,linestyle='-',color='blue',label=f'{0.2*100} pct. tax',zorder=-1);
     ax.plot(optimal_h_list,net_wage,lw=2,linestyle='-',color='black',label=f'{0.5*100} pct. tax',zorder=-1);
     ax.set_ylim([0,50]);
