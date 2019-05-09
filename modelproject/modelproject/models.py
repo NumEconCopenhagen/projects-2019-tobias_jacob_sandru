0734bbb0bfd7c5bf3fc37ffdaa78fb32ab2fb576
JacobOestermann <jacob_3400@hotmail.com>
2019-05-09 20:05:18 +0200

    Final

--- modelproject/modelproject/models.py
+++ modelproject/modelproject/models.py
@@ -24,12 +24,40 @@ def budget_cons_comp(h,u_b,u_a,tw=0.5,w=10,a=2,t0=1):
     '''The compensated budget constraint'''
     return (1-tw)*w*h-t0+a+(u_b-u_a)
 
-#define function for the utility function
-def utility_function(h,u,epsilon=-0.9):
-    '''The utility function'''
+#define function for the compensated consumption
+def comp_cons(h,u,epsilon=-0.9):
+    '''Compensated consumption'''
     return u-(1/(1+1/epsilon))*(1-h)**(1+1/epsilon)
 
+#Define the function for fincding the top tax
+def sol_func_top_tax(f,ta=0.2,tb=0.7,w=10,epsilon=-0.9,a=2,t0=1,K=5):
+    '''Top tax optimization'''
+    #Tax below
+    opt_h_below=f(tw=ta,epsilon=epsilon)
+    
+     #Tax above
+    opt_h_above=f(tw=tb,epsilon=epsilon)
+    
+    #Tax kink
+    opt_h_kink=K/w
 
+    if opt_h_below[0]*w<K:
+        opt_h=opt_h_below[0]
+       
+    elif opt_h_above[0]*w>K:
+        opt_h=opt_h_above[0]
+
+    else:
+        opt_h=opt_h_kink
+    
+    opt_x=(1-ta)*min(w*opt_h,K)+(1-tb)*max(w*opt_h-K,0)-t0+a
+    opt_u=opt_x+1/(1+1/epsilon)*(1-opt_h)**(1+1/epsilon)
+ 
+    return opt_h, opt_x, opt_u
+
+#Define budget line with top tax
+def budget_cons_top_tax(h,ta=0.2,tb=0.7,w=10,a=2,t0=1,K=5): 
+    return (1-ta)*min(w*h,K)+(1-tb)*max(w*h-K,0)-t0+a
 
 # _sol_func=sm.lambdify((tw,w,epsilon),sol[0])
 
--- modelproject/modelproject/visuals.py
+++ modelproject/modelproject/visuals.py
@@ -6,7 +6,7 @@ from .models import *
 
 def labor_supply_fig_func(epsilon, tw_0, tw_1, sol_func):
     '''This figure plots the labor supply, given two different tax rates (tw_0, tw_1) and the elasticity of labor supply (epsilon)'''
-    fig = plt.figure(figsize=(8,8));
+    fig = plt.figure(figsize=(6,6));
     ax = fig.add_subplot(1,1,1);
     wage_list = list(range(1,50))
     optimal_h_list = []
@@ -30,10 +30,10 @@ def labor_supply_fig_func(epsilon, tw_0, tw_1, sol_func):
             net_wage_1.append(wage-wage*0.2);
     ax.plot(optimal_h_list_1,net_wage_1,lw=2,linestyle='-',color='blue',label=f'{tw_0*100} pct. tax',zorder=-1);
     ax.plot(optimal_h_list,net_wage,lw=2,linestyle='-',color='black',label=f'{tw_1*100} pct. tax',zorder=-1);
-    ax.set_ylim([0,50]);
+    ax.set_ylim([0,40]);
     ax.set_xlim([0,1]);
     ax.legend(loc='lower right',fontsize=15);
     ax.set_xlabel('$h$',fontsize=20);
-    ax.set_ylabel('Net wage $(w-t \cdot w)$',fontsize=20);
+    ax.set_ylabel('Net wage $(1-t)w$',fontsize=20);
     ax.set_title(f'Labor supply \t $\epsilon$: {round(epsilon, 2)}',fontsize=30);
     return plt.show();
